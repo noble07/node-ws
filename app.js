bbf24c1ecea932cdaa43a63b67fe73fc18eec3ce
Juan Camilo <juancmz98@gmail.com>
2021-09-21 21:07:45 -0500

    Added: Serve static files with routing

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
 const express = require('express')
 const app = express()
 const port = 8080
- 
-app.get('/', (req, res) => {
-  res.send('Home Page')
-})
+
+// Servir contenido estático
+app.use(express.static('public'))
 
 app.get('/hola-mundo', (req, res) => {
   res.send('Hello World')
 })
 
+app.get('/elements', (req, res) => {
+  res.sendFile(__dirname + '/public/elements.html')
+})
+
+app.get('/generic', (req, res) => {
+  res.sendFile(__dirname + '/public/generic.html')
+})
+
 app.get('*', (req, res) => {
-  res.status(404).send('404 | Page not found')
+  res.sendFile(__dirname + '/public/404.html')
 })
  
 app.listen(port, () => {
