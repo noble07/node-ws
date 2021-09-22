a8b19d287734f1f2bfd2ef5709ce2072f369da11
Juan Camilo <juancmz98@gmail.com>
2021-09-21 22:33:20 -0500

    Added: Partials with HBS

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = 8080
 
 // Handlebars
 app.set('view engine', 'hbs');
-hbs.registerPartials(__dirname + '/views/partials', function (err) {});
+hbs.registerPartials(__dirname + '/views/partials');
 
 // Servir contenido estático
 app.use(express.static('public'))
@@ -19,11 +19,19 @@ app.get('/', (req, res) => {
 })
 
 app.get('/elements', (req, res) => {
-  res.sendFile(__dirname + '/public/elements.html')
+  res.render('elements', {
+    name: 'Juan Camilo Muñoz',
+    title: 'Curso de Node',
+    headerAlt: 'class="alt"'
+  })
 })
 
 app.get('/generic', (req, res) => {
-  res.sendFile(__dirname + '/public/generic.html')
+  res.render('generic', {
+    name: 'Juan Camilo Muñoz',
+    title: 'Curso de Node',
+    headerAlt: 'class="alt"'
+  })
 })
 
 app.get('*', (req, res) => {
